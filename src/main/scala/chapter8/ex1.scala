19360d5a16dd55a6524d3f0e9aa7e8809667e372
chdamianos <chdamianos@gmail.com>
2020-05-21 20:57:11 +0200

    chapter 8 ex1

---
+++ src/main/scala/chapter8/ex1.scala
@@ -0,0 +1,44 @@
+package chapter8
+
+object ex1 extends App {
+  val bnk1 = new BankAccount(initialBalance = 10.0)
+  println(bnk1.currentBalance)
+  bnk1.deposit(1.0)
+  println(bnk1.currentBalance)
+  val bnk2 = new CheckingAccount(initialBalance = 10.0)
+  println(bnk2.currentBalance)
+  bnk2.deposit(amount = 1.0)
+  println(bnk2.currentBalance)
+  bnk2.withdraw(amount = 1.0)
+  println(bnk2.currentBalance)
+}
+
+class BankAccount(initialBalance: Double) {
+  private var balance = initialBalance
+
+  def currentBalance: Double = balance
+
+  def deposit(amount: Double): Double = {
+    balance += amount;
+    balance
+  }
+
+  def withdraw(amount: Double): Double = {
+    balance -= amount;
+    balance
+  }
+}
+
+class CheckingAccount(initialBalance: Double) extends BankAccount(initialBalance: Double) {
+  private val chargeAmount = 1.0
+
+  override def deposit(amount: Double): Double = {
+    super.deposit(amount - chargeAmount)
+  }
+
+
+  override def withdraw(amount: Double): Double = {
+    super.withdraw(amount + chargeAmount)
+  }
+
+}
