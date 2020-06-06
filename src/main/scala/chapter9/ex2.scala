4828de51bee0f0947e13e18a4317c8dcbc264fe5
damianos <chdamianos@gmail.com>
2020-06-06 19:11:56 +0200

    chapter 9 ex2 couldnt do it

--- src/main/scala/chapter9/ex2.scala
+++ src/main/scala/chapter9/ex2.scala
@@ -5,7 +5,7 @@ import chapter9.ex1.getClass
 import scala.io.Source
 
 object ex2 extends App{
-
+/*couldn't make it work!*/
   val testFile = getClass.getResource("/chapter9ex2.txt").getPath
   val source = Source.fromFile(testFile, "UTF-8")
   val iter = source.buffered.mkString("\t")
